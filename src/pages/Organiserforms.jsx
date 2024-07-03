8e04f52d8a8e25a2423193c646f63ccd1baabc98
Harika <ruth.agbozo.93@gmail.com>
2024-07-03 16:38:34 +0000

    Add-Event

--- src/pages/Organiserforms.jsx
+++ src/pages/Organiserforms.jsx
@@ -2,72 +2,72 @@
 
 const Organiserforms = () => {
   return (
-    <div className="bg-red-500 bg-cover h-100vh">
-      <h1 className="text-white font-bold text-center">Create a New Event</h1>
+    <div className="bg-[#db6e1a] bg-cover h-100vh">
+      <h1 className="text-white font-bold text-3xl text-center">Create a New Event</h1>
      <form className=" flex flex-col max-w-md mx-auto">
           <div className="mb-8">
             <label
               htmlFor="name"
-              className="block text-black font-bold mb-1 ml-4"
+              className="block text-black font-medium mb-1 ml-4"
             >
               NAME
             </label>
             <input
               type="text"
-              className="h-10 w-full px-2 py-1 border-[#00B4BF] border-2 rounded-lg "
+              className="h-10 w-full px-2 py-1 border-[#fcba03] border-2 rounded-lg "
             />
           </div>
           <div className="mb-8">
             <label
               htmlFor="name"
-              className="block text-black font-bold mb-1 ml-4"
+              className="block text-black font-medium mb-1 ml-4"
             >
               PRICE
             </label>
             <input
               type="text"
-              className=" h-10 w-full px-2 py-1 border-[#00B4BF] border-2 rounded-lg"
+              className=" h-10 w-full px-2 py-1 border-[#fcba03] border-2 rounded-lg"
             />
           </div>
           <div className="mb-8">
             <label
               htmlFor="name"
-              className="block text-black font-bold mb-1 ml-4"
+              className="block text-black font-medium mb-1 ml-4"
             >
               IMAGE
             </label>
             <input
               type="file"
-              className="h-10 w-full px-2 py-1 border-[#00B4BF] border-2 rounded-lg"
+              className="h-10 w-full px-2 py-1 border-[#fcba03] border-2 rounded-lg"
             />
           </div>
           <div className="mb-8">
             <label
               htmlFor="name"
-              className="block text-black font-bold mb-1 ml-4"
+              className="block text-black font-medium mb-1 ml-4"
             >
               VENUE
             </label>
             <input
               type="text"
-              className="h-10 w-full px-2 py-1 border-[#00B4BF] border-2 rounded-lg"
+              className="h-10 w-full px-2 py-1 border-[#fcba03] border-2 rounded-lg"
             />
           </div>
           <div className="mb-8">
             <label
               htmlFor="name"
-              className="block text-black font-bold mb-1 ml-4"
+              className="block text-black font-medium mb-1 ml-4"
             >
               DATE
             </label>
             <input
               aria-label="Date"
               type="date"
-              className="h-10 w-full px-2 py-1 border-[#00B4BF] border-2 rounded-lg"
+              className="h-10 w-full px-2 py-1 border-[#fcba03] border-2 rounded-lg"
             />
           </div>
           <div className="mb-8">
-            <button type="submit" className="h-10 w-40 px-3 py-2 border-[#00B4BF] border-2 rounded-3xl"
+            <button type="submit" className="h-10 w-40 px-3 py-2 border-[#fcba03] border-2 rounded-3xl hover:bg-orange-100"
             >Create Event</button>
           </div>
         </form>
