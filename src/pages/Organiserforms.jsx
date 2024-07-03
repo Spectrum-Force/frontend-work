c92cfd7ad1d83cda60137e379a710fcc16c2a72d
Harika <ruth.agbozo.93@gmail.com>
2024-07-03 11:46:51 +0000

    Forms update

--- src/pages/Organiserforms.jsx
+++ src/pages/Organiserforms.jsx
@@ -2,12 +2,12 @@
 
 const Organiserforms = () => {
   return (
-    <div className="bg-red-500 bg-cover h-screen text-white">
-     <form className="max-w-lg mx-auto p-4">
+    <div className="bg-red-500 bg-cover h-100vh">
+     <form className=" flex flex-col max-w-md mx-auto">
           <div className="mb-8">
             <label
               htmlFor="name"
-              className="block text-gray-500 font-bold mb-1 ml-4"
+              className="block text-black font-bold mb-1 ml-4"
             >
               NAME
             </label>
@@ -21,7 +21,7 @@ const Organiserforms = () => {
           <div className="mb-8">
             <label
               htmlFor="name"
-              className="block text-gray-500 font-bold mb-1 ml-4"
+              className="block text-black font-bold mb-1 ml-4"
             >
               PRICE
             </label>
@@ -35,7 +35,7 @@ const Organiserforms = () => {
           <div className="mb-8">
             <label
               htmlFor="name"
-              className="block text-gray-500 font-bold mb-1 ml-4"
+              className="block text-black font-bold mb-1 ml-4"
             >
               IMAGE
             </label>
@@ -49,7 +49,7 @@ const Organiserforms = () => {
           <div className="mb-8">
             <label
               htmlFor="name"
-              className="block text-gray-500 font-bold mb-1 ml-4"
+              className="block text-black font-bold mb-1 ml-4"
             >
               VENUE
             </label>
@@ -63,16 +63,26 @@ const Organiserforms = () => {
           <div className="mb-8">
             <label
               htmlFor="name"
-              className="block text-gray-500 font-bold mb-1 ml-4"
+              className="block text-black font-bold mb-1 ml-4"
             >
               DATE
             </label>
             <input
-              type="text"
-              id="name"
+              aria-label="Date"
+              type="date"
               name="name"
               className="h-14 w-full px-3 py-2 border-[#00B4BF] border-2 rounded-lg"
             />
+          </div>
+          <div className="mb-8">
+            <button>
+            <input
+              type="submit"
+              id="onClick"
+              className="h-14 w-full px-3 py-2 border-[#00B4BF] border-2 rounded-lg"
+            />
+            </button>
+            
           </div>
         </form>
       
