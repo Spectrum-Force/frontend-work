e4d08840ea1dc594d2aba9f3192d7855011cb339
Harika <ruth.agbozo.93@gmail.com>
2024-07-03 10:11:43 +0000

    Forms

--- src/pages/Organiserforms.jsx
+++ src/pages/Organiserforms.jsx
@@ -1,8 +1,86 @@
-import React from 'react'
+
 
 const Organiserforms = () => {
   return (
-    <div>Organiserforms</div>
+    <div className="bg-red-500 bg-cover h-screen text-white">
+     <form className="max-w-lg mx-auto p-4">
+          <div className="mb-8">
+            <label
+              htmlFor="name"
+              className="block text-gray-500 font-bold mb-1 ml-4"
+            >
+              NAME
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="h-14 w-full px-3 py-2 border-[#00B4BF] border-2 rounded-lg "
+            />
+          </div>
+          <div className="mb-8">
+            <label
+              htmlFor="name"
+              className="block text-gray-500 font-bold mb-1 ml-4"
+            >
+              PRICE
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className=" h-14 w-full px-3 py-2 border-[#00B4BF] border-2 rounded-lg"
+            />
+          </div>
+          <div className="mb-8">
+            <label
+              htmlFor="name"
+              className="block text-gray-500 font-bold mb-1 ml-4"
+            >
+              IMAGE
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="h-14 w-full px-3 py-2 border-[#00B4BF] border-2 rounded-lg"
+            />
+          </div>
+          <div className="mb-8">
+            <label
+              htmlFor="name"
+              className="block text-gray-500 font-bold mb-1 ml-4"
+            >
+              VENUE
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="h-14 w-full px-3 py-2 border-[#00B4BF] border-2 rounded-lg"
+            />
+          </div>
+          <div className="mb-8">
+            <label
+              htmlFor="name"
+              className="block text-gray-500 font-bold mb-1 ml-4"
+            >
+              DATE
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="h-14 w-full px-3 py-2 border-[#00B4BF] border-2 rounded-lg"
+            />
+          </div>
+        </form>
+      
+      
+   
+ 
+      
+    </div>
   )
 }
 
