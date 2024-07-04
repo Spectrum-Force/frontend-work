625c327427b847235ed327431473fd06dafd871a
felix atoma <felixatoma2@gmail.com>
2024-07-04 13:59:22 +0000

    new changes

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 
 const SearchForm = () => {
-  return (
-
-		<div className='flex justify-center items-center pt-10 px-20'>
-      <form className='flex justify-between items-center gap-20'>
-        <div className='grid gap-2'>
+	return (
+			<form className='flex justify-between items-center gap-20 px-20'>
+				<div className='grid gap-2'>
 					<label className='text-[#6f6f6f] font-poppins font-semibold'>
 						SEARCH KEYWORD
 					</label>
 					<input
-						className='px-6 py-2 focus:outline-none border-solid border-2 border-slate-200 rounded-md' 
+						className='px-6 py-2 focus:outline-none border-solid border-2 border-slate-200 rounded-md'
 						type="text"
 						placeholder='SEARCH'
 					/>
@@ -41,7 +39,7 @@ const SearchForm = () => {
 					</select>
 				</div>
 				<div className='grid gap-2'>
-				<label className='text-[#6f6f6f] font-poppins font-semibold'>Event Months</label>
+					<label className='text-[#6f6f6f] font-poppins font-semibold'>Event Months</label>
 					<select className='px-6 py-2 focus:outline-none border-solid border-2 border-slate-200 rounded-md' name="location" id="">
 						<option value="kotobabi">All Dates</option>
 						<option value="kasoa">June,2024</option>
@@ -55,17 +53,16 @@ const SearchForm = () => {
 						<option value="Nima">February,2025</option>
 					</select>
 				</div>
-				<div>
+				<div className='mt-8'>
 					<button
 						className='px-8 py-2 border-solid border-2 border-[#ff6600] 
-							text-md text-[#ff6600] rounded-md hover:bg-[#ff6600] hover:text-white transition-all' 
+							text-md text-[#ff6600] rounded-md hover:bg-[#ff6600] hover:text-white transition-all'
 						type='submit'>
 						SEARCH
 					</button>
 				</div>
-      </form>
-    </div>
-  )
+			</form>
+	)
 }
 
 export default SearchForm
