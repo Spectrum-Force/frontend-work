93cb2b5541c94e2bbc9abd7216eebba0c5e9f060
Harika <ruth.agbozo.93@gmail.com>
2024-07-05 12:01:03 +0000

    Forms

--- src/pages/Organiserforms.jsx
+++ src/pages/Organiserforms.jsx
@@ -1,68 +1,84 @@
+import axios from "axios";
 
 
 const Organiserforms = () => {
+  // Post event to backend
+  const postEvent = async (event) => {
+      event.preventDefault();
+      // Collect all inputs from form
+      const formData = new FormData(event.target);
+      // Post data to backend
+      const response = await axios.post("https://backend-work-qwbo.onrender.com/events", formData);
+      console.log(response);
+  }
   return (
-    <div>
+
+    <div className="mt-10">
       <h1 className="text-black font-bold text-3xl text-center">Create a New Event</h1>
-     <form className=" flex flex-col max-w-md mx-auto">
+     <form onSubmit={postEvent} className=" flex flex-col max-w-md mx-auto">
           <div className="mb-8">
             <label
               htmlFor="name"
               className="block text-[#ef4e18] font-bold mb-1 ml-4"
             >
-              NAME
+              EVENT NAME
             </label>
             <input
               type="text"
+              id="name"
               className="h-10 w-full px-2 py-1 border-black border-2 rounded-lg "
             />
           </div>
           <div className="mb-8">
             <label
-              htmlFor="name"
+              htmlFor="price"
               className="block text-[#ef4e18] font-bold mb-1 ml-4"
             >
               PRICE
             </label>
             <input
               type="text"
+              id="price"
               className=" h-10 w-full px-2 py-1 border-black border-2 rounded-lg"
             />
           </div>
           <div className="mb-8">
             <label
-              htmlFor="name"
+              htmlFor="flyer"
               className="block text-[#ef4e18] font-bold mb-1 ml-4"
             >
               IMAGE
             </label>
             <input
               type="file"
+              id="flyer"
               className="h-10 w-full px-2 py-1 border-black border-2 rounded-lg"
             />
           </div>
           <div className="mb-8">
             <label
-              htmlFor="name"
+              htmlFor="location"
               className="block text-[#ef4e18] font-bold mb-1 ml-4"
             >
-              VENUE
+              LOCATION
             </label>
             <input
               type="text"
+              id="location"
               className="h-10 w-full px-2 py-1 border-black border-2 rounded-lg"
             />
           </div>
           <div className="mb-8">
             <label
-              htmlFor="name"
+              htmlFor="date"
               className="block text-[#ef4e18] font-bold mb-1 ml-4"
             >
               DATE
             </label>
             <input
-              aria-label="Date"
+              aria-label="date"
               type="date"
+              id="date"
               className="h-10 w-full px-2 py-1 border-black border-2 rounded-lg"
             />
           </div>
